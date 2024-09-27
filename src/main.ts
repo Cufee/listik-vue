4fd280700b5d868ff7fecc66331a2e34a633ff73
Cufee <vkouzin@outlook.com>
2024-09-27 17:11:48 -0400

    init

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
 
-const app = createApp(App)
+import App from "./App.vue";
+import router from "./router";
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+app.use(PrimeVue, {
+  theme: { preset: Aura, options: { darkModeSelector: ".dark-mode" } },
+});
+
+app.mount("#app");
